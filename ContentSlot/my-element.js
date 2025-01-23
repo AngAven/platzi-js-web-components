2358e5235d85ec8200f5ebc8a149f087c7f4b779
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-01-23 10:13:38 -0600

    Slot Content

---
+++ ContentSlot/my-element.js
@@ -0,0 +1,41 @@
+class myElement extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({mode: 'open'})
+    }
+
+    getTemplate() {
+        const template = document.createElement('template')
+            template.innerHTML = `
+            <section>
+                <h2 class="title"><slot></slot></h2>
+            </section>
+            ${this.getStyles()}
+        `
+        return template
+    }
+
+    getStyles() {
+        return `
+            <style>
+                .title{
+                    color: aqua;
+                } 
+                .text{
+                    color: dodgerblue;
+                }
+            </style>
+        `
+    }
+
+    render() {
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+}
+
+customElements.define('my-element', myElement)
+
